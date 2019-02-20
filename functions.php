1a1b8b20283ec26fc36d38ef0fee97a0f466b9db
sidd5sci <sidd5sci@gmail.com>
2019-02-20 12:34:07 -0800

    added some functions

--- functions.php
+++ functions.php
@@ -34,26 +34,135 @@ class phpUtils {
          * @param int|null  $parent_id
          * 
          */
-        public function multilevelItem($table,$parent_id){
+         private function multilevelItem($list,$parent_id){
+		$data = [];
+		
+		if(!$parent_id){
+			$lists = \App\Category::select('*')->where('parent',0)->get();
+		}
+		else{
+			$lists = \App\Category::select('*')->where('parent',$parent_id)->get();
+		}
+		foreach($lists as $item){
+			
+			if(!$parent_id){
+				$block = [
+					"name" => $item->name,
+					"path" => $item->path,
+					"indent" => 0,
+					"id" => $item->id,
+				];
+			}
+			elseif($item->parent == $parent_id){
+				$block = [
+					"name" => $item->name,
+					"path" => $item->path,
+					"indent" => 3,
+					"id" => $item->id,
+				];
+			}
+			
+			
+				$block["childs"] = $this->multilevelItem($item->id);
+				$data[] = $block;
+				// dd($c);
+			}
+			return $data;
+		}
+		/**
+		 * file upload
+		 */
+		private function uploadFile($t_dir,$basename,$tempname,$name){
+			
+			// $c = explode('.',basename($_FILES['resume']["name"]);
+			$target_dir = $t_dir;
+			$target_file = "file_".time().'_'.$this->random_().'.'.$basename->getClientOriginalExtension();
+			$uploadOk = 1;
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+				// echo "<br>Sorry, your file was not uploaded.";
+				// if everything is ok, try to upload file
+			} else {
+				if (move_uploaded_file($tempname, $t_dir.$target_file)) {
+				//   echo "<br>The file ". $basename. " has been uploaded.";
+				return $target_file;
+				} else {
+				//   echo "<br>Sorry, there was an error uploading your file.";
+				}
+			}
+		}
+		/**
+		 *  send mail
+		 */
+		private function sendMail($to,$from,$custom_from,$subject,$content,$senderName){
+			
+			// To send HTML mail, the Content-type header must be set
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+			
+			// fetching email first
+			$name = strval($senderName);
+			// Create email headers
+			$headers .= 'From: '.$name.' <'.$from.'>'."\r\n".
+					'Reply-To: '.$custom_from."\r\n".
+					'X-Mailer: PHP/' . phpversion();
+			$headers .= "Content-Type: multipart/mixed"."\r\n"; // Defining Content-Type 
+			$headers .= "Sender: ".$custom_from."\r\n";
+			// $headers .= "boundary = $boundary\r\n"; //Defining the Boundary
+			
+			// Compose a simple HTML email message
+			
+			// Sending email
+			if(mail($to, $subject, $content, $headers)){
+				// echo '<br>Your mail has been sent successfully.';
+				return true;
+			} else{
+				//     echo '<br>Unable to send email. Please try again.';
+				return false;
+			}
+		
+		}
+		/**
+		 *  Checkbox group itrator
+		 */
+		function itirator($aDoor){
+			$sequence = '';
+			if(empty($aDoor)) {
+				// echo("You didn't select any buildings.");
+			} 
+			else{
+				$N = count($aDoor);
+				//echo("You selected $N door(s): ");
+				for($i=0; $i < $N; $i++){
+					// echo($aDoor[$i] . " ");
+					$sequence .= $aDoor[$i].', ';
+				}
+			}
+			return $sequence;
+		}
+		/**
+		 *  Random
+		 */
+		public function random_(){
+			// Available alpha caracters
+			$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-                if(is_null($parent_id)){
-                        $lists = DB::table('users')->select('*')->where('parent',null)->get();
-                }
-                else{
-                        $lists = DB::table('users')->select('*')->where('parent',$parent_id)->get();
-                }
-                foreach($lists as $item){
-                        if(is_null($parent_id)){
-                                echo '<li>'.$item->name.'</li>';
-                        }
-                        else{
-                                echo '<li class="indent">'.$item->name.'</li>';
-                        }
-                        $this->multilevelItem($table,$parent_id);
-                }
-        }
-        public function sendMail(){}
-        public function itirator(){}
-        public function randomNumStr(){}
+			// generate a pin based on 2 * 7 digits + a random character
+			$pin = mt_rand(1000000, 9999999)
+				. mt_rand(1000000, 9999999)
+				. $characters[rand(0, strlen($characters) - 1)];
+
+			// shuffle the result
+			$string = str_shuffle($pin);
+			return $string;
+		}
+	public function protectImgUrl($url,$id){
+		$name = random_str(10);
+		$name .= '.jpe';
+		// https://www.codeproject.com/Articles/6814/Securing-image-URLs-in-a-website
+		// https://stackoverflow.com/questions/5487116/restricting-images-from-direct-url-download
+		// https://css-tricks.com/techniques-for-fighting-image-theft/
+	}
+
 
 }
