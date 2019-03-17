972d02f82c28cfa037b3503ffca434e93feab9f9
sidd5sci <sidd5sci@gmail.com>
2019-03-17 19:16:21 +0530

    created the db function read records any number

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?
+require '../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
 $DB = new DB;
@@ -8,7 +9,7 @@ $DB->addConnection([
     'host'      => 'localhost',
     'database'  => 'database',
     'username'  => 'root',
-    'password'  => 'password',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
@@ -23,7 +24,7 @@ $DB->setEventDispatcher(new Dispatcher(new Container));
 $DB->setAsGlobal();
 
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsDBule->bootEloquent();
+$DB->bootEloquent();
 
 
 class phpUtils {
@@ -34,34 +35,34 @@ class phpUtils {
          * @param int|null  $parent_id
          * 
          */
-         private function multilevelItem($list,$parent_id){
-		$data = [];
-		
-		if(!$parent_id){
-			$lists = \App\Category::select('*')->where('parent',0)->get();
-		}
-		else{
-			$lists = \App\Category::select('*')->where('parent',$parent_id)->get();
-		}
-		foreach($lists as $item){
+		private function multilevelItem($list,$parent_id){
+			$data = [];
 			
 			if(!$parent_id){
-				$block = [
-					"name" => $item->name,
-					"path" => $item->path,
-					"indent" => 0,
-					"id" => $item->id,
-				];
+				$lists = DB::select('*')->where('parent',0)->get();
 			}
-			elseif($item->parent == $parent_id){
-				$block = [
-					"name" => $item->name,
-					"path" => $item->path,
-					"indent" => 3,
-					"id" => $item->id,
-				];
+			else{
+				$lists = DB::select('*')->where('parent',$parent_id)->get();
 			}
-			
+			foreach($lists as $item){
+				
+				if(!$parent_id){
+					$block = [
+						"name" => $item->name,
+						"path" => $item->path,
+						"indent" => 0,
+						"id" => $item->id,
+					];
+				}
+				elseif($item->parent == $parent_id){
+					$block = [
+						"name" => $item->name,
+						"path" => $item->path,
+						"indent" => 3,
+						"id" => $item->id,
+					];
+				}
+				
 			
 				$block["childs"] = $this->multilevelItem($item->id);
 				$data[] = $block;
@@ -156,13 +157,40 @@ class phpUtils {
 			$string = str_shuffle($pin);
 			return $string;
 		}
-	public function protectImgUrl($url,$id){
-		$name = random_str(10);
-		$name .= '.jpe';
-		// https://www.codeproject.com/Articles/6814/Securing-image-URLs-in-a-website
-		// https://stackoverflow.com/questions/5487116/restricting-images-from-direct-url-download
-		// https://css-tricks.com/techniques-for-fighting-image-theft/
-	}
+		public function protectImgUrl($url,$id){
+			$name = random_str(10);
+			$name .= '.jpe';
+			// https://www.codeproject.com/Articles/6814/Securing-image-URLs-in-a-website
+			// https://stackoverflow.com/questions/5487116/restricting-images-from-direct-url-download
+			// https://css-tricks.com/techniques-for-fighting-image-theft/
+		}
 
-
+		/**
+		 * display number rows count ant anchor
+		 * @param number | integer  
+		 * @param anchor | integer
+		 */
+		// $data = array(
+		// 	'conn' => $link,
+		// 	'table' => $tbalename,
+		// 	'abc' =>  $abc,
+		// );
+		public function displayRecords($start = 0,$number = 1,$anchor = 0,$args){
+			if($anchor == 0){
+				// select * from table limit $number
+				// $q = "select * from $args[table] limit $number"
+				$records = DB::select('*')->limit($number)->get();
+				return $records;
+			}
+			elseif($anchor == 1){
+				// select * from tbl_name order by id desc limit N
+				$records = DB::select('*')->orderby('id','DESC')->limit($number)->get();
+				return $records;
+			}
+			else{
+				//SELECT * FROM `student` WHERE id > $start limit $number
+				$records = DB::select('*')->where('id','>',$start)->limit($number)->get();
+				return $records;
+			}
+		}
 }
